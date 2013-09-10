5df6980ffbb0c1046b1353bd29946a86c2b4694c
Peter Clarke <petermclarke@gmail.com>
2013-09-10 22:11:50 +0100

    Symfony coding standard

--- DeviceDetector.php
+++ DeviceDetector.php
@@ -13,164 +13,170 @@ use Symfony\Component\Console\Output\OutputInterface;
 class DeviceDetector
 {
 
-  /**
-   * The devices to scan
-   */
-  protected $devices;
+    /**
+    * The devices to scan
+    */
+    protected $devices;
 
-  /**
-   * Devices that have been detected
-   */
-  protected $detected;
+    /**
+    * Devices that have been detected
+    */
+    protected $detected;
 
-  /**
-   * The OutputInterface object
-   */
-  protected $output;
+    /**
+    * The OutputInterface object
+    */
+    protected $output;
 
-  /**
-   * A Symfony\Component\Process object
-   */
-  protected $process;
+    /**
+    * A Symfony\Component\Process object
+    */
+    protected $process;
 
-  /**
-   * Constructor
-   */
-  public function __construct($devices, $process) {
-    $this->devices = $devices;
-    $this->process = $process;
-    $this->found = array();
+    /**
+    * Constructor
+    */
+    public function __construct($devices, $process)
+    {
+        $this->devices = $devices;
+        $this->process = $process;
+        $this->found = array();
 
-    try {
-      $this->validateDevices();
-    } catch (\InvalidArgumentException $e) {
-      throw $e;
-    }
-
-  }
-
-  public function setOutput(OutputInterface $output) {
-    $this->output = $output;
-  }
-
-  public function setEntityManager(EntityManager $em) {
-    $this->em = $em;
-  }
-
-  /**
-   * Detect the the devices of interest.
-   *
-   * @return bool
-   */
-  public function detect() {
-    // are the phones on the network...
-    $str = $this->scan();
-    $present = $this->analyseScan($str);
-    if (!$present) {
-      // repeat in a few seconds because the phones take a while to respond initially...
-      sleep(10);
-      $str = $this->scan();
-      $present = $this->analyseScan($str);
-    }
-
-    if (!empty($this->output)) {
-      if ($present) {
-        $out = 'Detected:';
-        foreach ($this->detected as $detected) {
-          $out .= ' <info>' . $detected . '</info>';
+        try {
+            $this->validateDevices();
+        } catch (\InvalidArgumentException $e) {
+            throw $e;
         }
-        $devices = join(' ', $this->detected);
-      } else {
-        $out = 'Devices: <info>' . $this->devices . '</info> not found';
-        $devices = '';
-      }
-      $this->output->writeln($out);
+
     }
 
-    // TMP store in db
-    if (isset($this->em)) {
-      $detection = new Detection();
-      $detection->setTimestamp(new \DateTime());
-      $detection->setDevices($devices);
-
-      try {
-        $this->em->persist($detection);
-        $this->em->flush();
-      } catch (\PDOException $e) {
-          // ignore for now
-      }
+    public function setOutput(OutputInterface $output)
+    {
+        $this->output = $output;
     }
 
-    return $present;
-  }
-
-  /**
-   * Scan the network for attached devices
-   *
-   * @throws \RuntimeException
-   * @return string
-   */
-  public function scan() {
-
-    $cmd = 'nmap -sP ' . $this->devices . ' 2>&1';
-
-    $this->process->setCommandLine($cmd);
-    $this->process->run();
-    if (!$this->process->isSuccessful()) {
-      throw new \RuntimeException($this->process->getErrorOutput());
-    }
-    $output = $this->process->getOutput();
-    return trim($output);
-  }
-
-  /**
-   * Analyse the output of the scan to find the relevant info.
-   *
-   * @return bool
-   */
-  public function analyseScan($str) {
-
-    // output results if verbose (app/console -v cctvbf:detect)
-    if (!empty($this->output) && $this->output->getVerbosity() > 1) {
-      $this->output->writeln($str);
+    public function setEntityManager(EntityManager $em)
+    {
+        $this->em = $em;
     }
 
-    if (strstr($str, '(0 hosts up)')) {
-      // no devices found
-      return false;
+    /**
+    * Detect the the devices of interest.
+    *
+    * @return bool
+    */
+    public function detect()
+    {
+        // are the phones on the network...
+        $str = $this->scan();
+        $present = $this->analyseScan($str);
+        if (!$present) {
+            // repeat in a few seconds because the phones take a while to respond initially...
+            sleep(10);
+            $str = $this->scan();
+            $present = $this->analyseScan($str);
+        }
+
+        if (!empty($this->output)) {
+            if ($present) {
+                $out = 'Detected:';
+                foreach ($this->detected as $detected) {
+                    $out .= ' <info>' . $detected . '</info>';
+                }
+                $devices = join(' ', $this->detected);
+            } else {
+                $out = 'Devices: <info>' . $this->devices . '</info> not found';
+                $devices = '';
+            }
+            $this->output->writeln($out);
+        }
+
+        // TMP store in db
+        if (isset($this->em)) {
+            $detection = new Detection();
+            $detection->setTimestamp(new \DateTime());
+            $detection->setDevices($devices);
+
+            try {
+                $this->em->persist($detection);
+                $this->em->flush();
+            } catch (\PDOException $e) {
+                // ignore for now
+            }
+        }
+
+        return $present;
     }
 
-    // Note which ones were found
-    $devices = explode(' ', $this->devices);
+    /**
+    * Scan the network for attached devices
+    *
+    * @throws \RuntimeException
+    * @return string
+    */
+    public function scan()
+    {
+        $cmd = 'nmap -sP ' . $this->devices . ' 2>&1';
 
-    foreach ($devices as $ip) {
-      if (strstr($str, '(' . $ip . ')')) {
-        $this->detected[] = $ip;
-      }
+        $this->process->setCommandLine($cmd);
+        $this->process->run();
+        if (!$this->process->isSuccessful()) {
+            throw new \RuntimeException($this->process->getErrorOutput());
+        }
+        $output = $this->process->getOutput();
+
+        return trim($output);
     }
 
-    return true;
-  }
+    /**
+    * Analyse the output of the scan to find the relevant info.
+    *
+    * @return bool
+    */
+    public function analyseScan($str)
+    {
 
-  protected function validateDevices() {
-    // Expects space seperated ip addresses.
-    //
-    // theapi_cctv_blindfold:
-    //     devices: 192.168.0.112 192.168.0.118
-    $devices = explode(' ', $this->devices);
-    $ips = array();
-    foreach ($devices as $ip) {
-      if ($valid = filter_var($ip, FILTER_VALIDATE_IP)) {
-        $ips[] = $valid;
-      }
+        // output results if verbose (app/console -v cctvbf:detect)
+        if (!empty($this->output) && $this->output->getVerbosity() > 1) {
+            $this->output->writeln($str);
+        }
+
+        if (strstr($str, '(0 hosts up)')) {
+            // no devices found
+            return false;
+        }
+
+        // Note which ones were found
+        $devices = explode(' ', $this->devices);
+
+        foreach ($devices as $ip) {
+            if (strstr($str, '(' . $ip . ')')) {
+                $this->detected[] = $ip;
+            }
+        }
+
+        return true;
     }
 
-    if (empty($ips)) {
-      $this->devices = null;
-      throw new \InvalidArgumentException('No valid ip addresses provided for theapi_cctv_blindfold:devices');
+    protected function validateDevices()
+    {
+        // Expects space seperated ip addresses.
+        //
+        // theapi_cctv_blindfold:
+        //     devices: 192.168.0.112 192.168.0.118
+        $devices = explode(' ', $this->devices);
+        $ips = array();
+        foreach ($devices as $ip) {
+            if ($valid = filter_var($ip, FILTER_VALIDATE_IP)) {
+                $ips[] = $valid;
+            }
+        }
+
+        if (empty($ips)) {
+            $this->devices = null;
+            throw new \InvalidArgumentException('No valid ip addresses provided for theapi_cctv_blindfold:devices');
+        }
+
+        $this->devices = join(' ', $ips);
     }
-
-    $this->devices = join(' ', $ips);
-  }
-
 }
