cccec512cec5df97655f833f75181c5ca23f31b0
Peter Clarke <petermclarke@gmail.com>
2013-09-25 20:35:55 +0100

    Update interupt.py
    Pull up

--- python/interupt.py
+++ python/interupt.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python2.7
-# @see http://RasPi.tv
+# @see http://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio
 
-import signal, sys
+import signal, sys, time
 import RPi.GPIO as GPIO
+
+
 GPIO.setmode(GPIO.BCM)
 
-# GPIO 23 set up as inputs. Pulled down.
-# 23 will go to 3V3 (3.3V)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# GPIO 23 set up as inputs.
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 def signal_handler(signal, frame):
@@ -18,18 +19,14 @@ def signal_handler(signal, frame):
 # now we'll define the threaded callback function
 # this will run in another thread when our event is detected
 def my_callback(channel):
-    print "Rising edge detected on port 23 - even though, in the main thread,"
-    print "- how cool?\n"
+    print(time.ctime())
 
-print "You will also need a second button connected so that when pressed"
-print "it will connect GPIO port 23 (pin 18) to 3V3 (pin 1)"
+print "started..."
 
 
 # The GPIO.add_event_detect() line below set things up so that
-# when a rising edge is detected on port 23, regardless of whatever
-# else is happening in the program, the function "my_callback" will be run
 # It will happen even while the program is in the loop.
-GPIO.add_event_detect(23, GPIO.RISING, callback=my_callback, bouncetime=200)
+GPIO.add_event_detect(23, GPIO.FALLING, callback=my_callback, bouncetime=200)
 
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -38,6 +35,5 @@ print 'Press Ctrl+C to exit'
 while 1:
     # listen to sockets, adc etc
     time.sleep(10) # just wait a while for now.
-
 
 
