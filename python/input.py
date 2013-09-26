13a6cdd27ca6c07f406db4512c10b9b0ac50fbba
Peter <petermclarke@gmail.com>
2013-09-26 12:55:11 +0100

    Use an interupt to listen for the switch push

--- python/input.py
+++ python/input.py
@@ -3,14 +3,20 @@
 # Thanks to http://www.binarytides.com/code-chat-application-server-client-sockets-python/
 
 import socket, select, atexit, signal, sys
-import RPi.GPIO as io
+import RPi.GPIO as GPIO
 from time import sleep
 from Adafruit_ADS1x15 import ADS1x15
 
 
 def signal_handler(signal, frame):
     print 'You pressed Ctrl+C!'
+    GPIO.cleanup()  # clean up GPIO on exit
     sys.exit(0)
+
+# Called when the switch is pushed
+def switch_callback(channel):
+    print("TOGGLE")
+    broadcast_data('TOGGLE')
 
 def broadcast_data (message):
     #Do not send the message to master socket and the client who has send us the message
@@ -25,42 +31,41 @@ def broadcast_data (message):
 
 if __name__ == "__main__":
 
-    io.setmode(io.BCM)
-    
+    GPIO.setmode(GPIO.BCM)
+
     switch_pin = 23
-    io.setup(switch_pin, io.IN, pull_up_down=io.PUD_DOWN)  # activate input with Pull Down - Ov
-	
+    # set up as input with pull up.
+    GPIO.setup(switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
     signal.signal(signal.SIGINT, signal_handler)
     #print 'Press Ctrl+C to exit'
-    
+
     ADS1015 = 0x00  # 12-bit ADC
     ADS1115 = 0x01  # 16-bit ADC
-     
+
     # Initialise the ADC using the default mode (use default I2C address)
     # Set this to ADS1015 or ADS1115 depending on the ADC you are using!
     adc = ADS1x15(ic=ADS1015)
-     
+
     # List to keep track of socket descriptors
     CONNECTION_LIST = []
     RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
     PORT = 8889
-     
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(("0.0.0.0", PORT))
     server_socket.listen(1)
- 
+
     # Add server socket to the list of readable connections
     CONNECTION_LIST.append(server_socket)
 
+    # The GPIO.add_event_detect() line below set things up so that
+    # the callback will happen even while the program is in the loop.
+    GPIO.add_event_detect(switch_pin, GPIO.FALLING, callback=switch_callback, bouncetime=200)
 
     while 1:
-      
-        # Check the toggle switch for a press
-        if io.input(door_pin):
-            print("TOGGLE")
-            broadcast_data('TOGGLE')
-      
+
         # Read channel 0 in single-ended mode, +/-4.096V, 250sps
         #reading = adc.readADCSingleEnded(3, 1024, 860)
 
@@ -69,18 +74,18 @@ if __name__ == "__main__":
         #    broadcast_data(reading)
 
         #time.sleep(0.01)
-		
+
         # Get the list sockets which are ready to be read through select
         read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[],0.01)
-			
-      	for sock in read_sockets:
+
+        for sock in read_sockets:
             #New connection
             if sock == server_socket:
                 # Handle the case in which there is a new connection recieved through server_socket
                 sockfd, addr = server_socket.accept()
                 CONNECTION_LIST.append(sockfd)
                 print "Client (%s, %s) connected" % addr
-        		
+
             #Some incoming message from a client
             else:
                 # Data recieved from client, process it
@@ -88,13 +93,13 @@ if __name__ == "__main__":
                     # In Windows, sometimes when a TCP program closes abruptly,
                     # a "Connection reset by peer" exception will be thrown
                     data = sock.recv(RECV_BUFFER)
-                    # ignore it, this is broadcast only           
-        			 
+                    # ignore it, this is broadcast only
+
                 except:
                     print "Client (%s, %s) is offline" % addr
                     sock.close()
                     CONNECTION_LIST.remove(sock)
                     continue
-			
-	
-			
+
+
+
