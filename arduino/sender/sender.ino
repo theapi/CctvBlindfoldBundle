aeb16acdb1cfdd0cfc9b55aa46968daec5612815
Peter Clarke <petermclarke@gmail.com>
2013-12-23 23:07:34 +0000

    Tidy up

--- arduino/sender/sender.ino
+++ arduino/sender/sender.ino
@@ -1,12 +1,12 @@
-// transmitter.pde
-//
-// Simple example of how to use VirtualWire to transmit messages
-// Implements a simplex (one-way) transmitter with an TX-C1 module
-//
-// See VirtualWire.h for detailed API docs
-// Author: Mike McCauley (mikem@airspayce.com)
-// Copyright (C) 2008 Mike McCauley
-// $Id: transmitter.pde,v 1.3 2009/03/30 00:07:24 mikem Exp $
+/*
+ One PIR sensor detects motion at the bottom of the stairs: DOWN.
+ Another detects motion at the top of the stairs: UP.
+ 
+ Sends an RF message when one both sensors have been triggered,
+ indicating the direction of travel.
+ The message is sent multiple times to give it the best chance of getting through.
+*/
+
 #include <VirtualWire.h>
 
 
@@ -14,8 +14,8 @@
 #define RF_TX_PIN 12    // Pin 12 is the default sender pin.
 #define DEBUG_LED_TX 13 // Debug led for tx
 
-#define PIR_UP 2        // int.0 interupt 0 on pin 2
-#define PIR_DOWN 3      // int.1 interupt 1 on pin 3
+#define PIR_UP_PIN 2        // int.0 interupt 0 on pin 2
+#define PIR_DOWN_PIN 3      // int.1 interupt 1 on pin 3
 
 // Bit flags for pir state
 #define F_UP 1          // 1 = activated  0 = no movement
@@ -28,75 +28,66 @@ unsigned long count = 0;
 long previousMillis = 0;
 long interval = 1000; 
 
-long pirReset = 4000;
+long pirReset = 3000; // How long to ignore further detects.
 long lastPirUp = 0;
 long lastPirDown = 0;
 
 // Store the pir states on interupt.
 volatile byte flags = 0;
 
-void pirUp() 
+// Interupt on PIR_UP_PIN pin gone high.
+void isrPirUp() 
 {
   // Motion detected.
   bitSet(flags, F_UP);
   bitSet(flags, F_UP_STAT);
-  Serial.println("UP detected");
-  /*
-   if (digitalRead(PIR_UP) == LOW) { // falling
-     // No more motion.
-     bitClear(flags, F_UP);
-     Serial.println("UP off");
-   } else {
-     // Motion detected.
-     bitSet(flags, F_UP);
-     Serial.println("UP detected");
-   }
-   */
+  
+  // Cannot detect both at once, so see if the other fired too.
+  if (!bitRead(flags, F_DOWN_STAT)) {
+    if (digitalRead(PIR_DOWN_PIN) == HIGH) {
+      isrPirDown();
+    }
+  }
+  
 }
 
-void pirDown() 
+// Interupt on PIR_DOWN_PIN pin gone high.
+void isrPirDown() 
 {
   // Motion detected.
   bitSet(flags, F_DOWN);
   bitSet(flags, F_DOWN_STAT);
-  Serial.println("DOWN detected");
-     
-  /*
-   if (digitalRead(F_DOWN) == LOW) { // falling
-     // No more motion.
-     bitClear(flags, F_DOWN);
-     Serial.println("DOWN off");
-   } else {
-     // Motion detected.
-     bitSet(flags, F_DOWN);
-     Serial.println("DOWN detected");
-     //Serial.println(bitRead(flags, F_DOWN));
-   }
-   */
+  
+  // Cannot detect both at once, so see if the other fired too.
+  if (!bitRead(flags, F_UP_STAT)) {
+    if (digitalRead(PIR_UP_PIN) == HIGH) {
+      isrPirUp();
+    }
+  }
 }
 
 void setup()
 {
     pinMode(DEBUG_LED_TX, OUTPUT);
     
-    pinMode(PIR_UP, INPUT);     // declare sensor as input
-    pinMode(PIR_DOWN, INPUT);
+    pinMode(PIR_UP_PIN, INPUT);
+    pinMode(PIR_DOWN_PIN, INPUT);
     
     bitClear(flags, F_UP);
     bitClear(flags, F_DOWN);
+    bitClear(flags, F_UP_STAT);
+    bitClear(flags, F_DOWN_STAT);
      
   
     Serial.begin(9600);
     Serial.println("setup");
     
     vw_set_tx_pin(RF_TX_PIN);
-    // Initialise the IO and ISR
-    vw_set_ptt_inverted(true); // Required for DR3100
     vw_setup(2000);      // Bits per sec
     
     
-    attachInterrupt(0, pirUp, RISING);
-    attachInterrupt(1, pirDown, RISING);
+    attachInterrupt(0, isrPirUp, RISING);
+    attachInterrupt(1, isrPirDown, RISING);
 
 }
 
@@ -111,11 +102,13 @@ void loop()
     // Can't set use millis in the interupt.
     if (bitRead(flags, F_UP_STAT)) {
       lastPirUp = currentMillis;
+      Serial.println("UP on");
       bitClear(flags, F_UP_STAT);
     }
     
     if (bitRead(flags, F_DOWN_STAT)) {
       lastPirDown = currentMillis;
+      Serial.println("DOWN on");
       bitClear(flags, F_DOWN_STAT);
     }
 
@@ -133,10 +126,7 @@ void loop()
     if (bitRead(flags, F_UP) || bitRead(flags, F_DOWN)) { 
       char buf[50];
       sprintf(buf, "count=%lu,flags=%u", count, flags); 
-    
-      //Serial.println(bitRead(flags, F_DOWN));
-      //Serial.println(bitRead(flags, F_UP));
-    
+   
       if (bitRead(flags, F_UP)) { 
         strcat (buf, " UP ");
       }
@@ -147,7 +137,6 @@ void loop()
         
       Serial.println(buf);
     
-      //const char *buf = "hello";
       digitalWrite(DEBUG_LED_TX, true); // Flash a light to show transmitting
       vw_send((uint8_t *)buf, strlen(buf));
       vw_wait_tx(); // Wait until the whole message is gone
